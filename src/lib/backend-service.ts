851edce7139b28291778bacc26abdcc1ca81dcfd
abar-1 <aneeshbargaje@gmailcom>
2025-09-20 15:47:32 -0500

    initial changes

---
+++ src/lib/backend-service.ts
@@ -0,0 +1,8 @@
+export const backendService = {
+  isUsingSupabase(): boolean {
+    return !!(
+      process.env.NEXT_PUBLIC_SUPABASE_URL && 
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    )
+  }
+}
